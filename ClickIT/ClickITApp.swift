c25e2e06738b37f4999ab285b992ff41d02b13a4
Himanshu Pharawal <himanshupharawal100@gmail.com>
2025-06-04 10:44:07 +0530

    Initial Commit

---
+++ ClickIT/ClickITApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickITApp.swift
+//  ClickIT
+//
+//  Created by Himanshu Pharawal on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ClickITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
